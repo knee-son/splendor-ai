ab978b911b5bc0245cabbe605c4dd06b0a618a75
knee-son <okaokaloid@gmail.com>
2025-10-21 04:40:19 +0800

    init initial game state

--- frontend-ministry/src/pages/InitialGameStatePage.tsx
+++ frontend-ministry/src/pages/InitialGameStatePage.tsx
@@ -1,19 +1,28 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import type { Card, Noble } from '@/types/splendor';
 import NobleCard from '@/components/NobleCard';
 import EngineCard from '@/components/NobleCard';
 
 export default function InitialGameStatePage() {
   const navigate = useNavigate();
 
-  const [nobles, setNobles] = useState<any[]>([]);
-  const [cards, setCards] = useState<any[]>([]);
+  const cards_url = import.meta.env.VITE_CARDS_URL;
+  const nobles_url = import.meta.env.VITE_NOBLES_URL;
+  const init_url = import.meta.env.VITE_INIT_URL;
+
+  const [nobles, setNobles] = useState<Noble[]>([]);
+  const [cards, setCards] = useState<Card[]>([]);
+  const [gameState, setGameState] = useState<string>('Loading board...');
+
+  function fetchInitialState() {
+    fetch(init_url)
+      .then(res => res.json())
+      .then(data => setGameState(data.ascii));
+  }
 
   useEffect(() => {
-    const cards_url = import.meta.env.VITE_CARDS_URL;
-    const nobles_url = import.meta.env.VITE_NOBLES_URL;
-
     fetch(cards_url)
       .then(res => res.json())
       .then(data => setCards(data));
@@ -21,6 +30,8 @@ export default function InitialGameStatePage() {
     fetch(nobles_url)
       .then(res => res.json())
       .then(data => setNobles(data));
+
+    fetchInitialState();
   }, []);
 
   return (
@@ -33,12 +44,19 @@ export default function InitialGameStatePage() {
       Back to Menu
     </button>
     <button
-      onClick={() => {}}
+      onClick={fetchInitialState}
       className="flex items-center p-3 bg-gray-500 text-white rounded-md absolute top-5 right-5 hover:bg-gray-600"
     >
       <span className="material-symbols-outlined mr-3">casino</span>
       Reshuffle Board
     </button>
+
+    <div
+      className="bg-[#f5deb3] text-gray-800 font-mono w-6/12 aspect-square flex items-center justify-center p-8 rounded-lg shadow-md max-w-xl text-center border border-amber-700"
+      style={{ whiteSpace: 'pre-wrap' }}
+    >
+      {gameState}
+    </div>
 
   </div>
   );
--- frontend-ministry/src/routes/index.tsx
+++ frontend-ministry/src/routes/index.tsx
@@ -1,9 +1,17 @@
+import type { ReactElement } from "react";
+
 import LandingPage from "@/pages/LandingPage";
 import CardsPage from "@/pages/CardsPage";
 import NoblesPage from "@/pages/NoblesPage";
 import InitialGameStatePage from "@/pages/InitialGameStatePage";
 
-export const appRoutes = [
+interface AppRoute  {
+    label: string,
+    path: string,
+    element: ReactElement,
+}
+
+export const appRoutes: AppRoute[] = [
   { label: "Home", path: "/", element: <LandingPage /> },
   { label: "Check Splendor Cards", path: "/cards", element: <CardsPage /> },
   { label: "Check Splendor Nobles", path: "/nobles", element: <NoblesPage /> },
