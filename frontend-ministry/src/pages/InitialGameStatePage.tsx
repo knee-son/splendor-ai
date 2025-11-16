411f9436de2671b46c08f27ba319b96c205ef756
knee-son <okaokaloid@gmail.com>
2025-11-17 06:31:16 +0800

    redesign board for initial game state page

--- frontend-ministry/src/pages/InitialGameStatePage.tsx
+++ frontend-ministry/src/pages/InitialGameStatePage.tsx
@@ -6,44 +6,88 @@
  * @page
  */
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-import type { Card, Noble, State } from "@/types/splendor";
+import type { State, TierKey } from "@/types/splendor";
 import NobleCard from "@/components/NobleCard";
-import EngineCard from "@/components/NobleCard";
+import EngineCard from "@/components/EngineCard";
+import EngineUnderside from "@/components/EngineUnderside";
 
 export default function InitialGameStatePage() {
   const navigate = useNavigate();
 
-  const cards_url = import.meta.env.VITE_CARDS_URL;
-  const nobles_url = import.meta.env.VITE_NOBLES_URL;
   const init_url = import.meta.env.VITE_INIT_URL;
 
-  const [nobles, setNobles] = useState<Noble[]>([]);
-  const [cards, setCards] = useState<Card[]>([]);
-  const [gameState, setGameState] = useState<State | null>(null);
+  const nobleRefs = useRef<(HTMLDivElement | null)[]>([]);
 
-  function fetchInitialState() {
-    fetch(`${init_url}?get-ascii`)
+  const cardRefs = useRef<(HTMLDivElement | null)[][]>([]);
+  const undersideRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const [gameState, setGameState] = useState<State | null>(null);
+  const [isFetching, setIsFetching] = useState<boolean>(true);
+
+  async function fetchInitialState() {
+    setIsFetching(true);
+
+    await fetch(`${init_url}?get-ascii`)
       .then((res) => res.json())
       .then((data) => {
         setGameState(data.state);
-        console.log(data.state);
       });
+
+    setIsFetching(false);
   }
 
   useEffect(() => {
-    fetch(cards_url)
-      .then((res) => res.json())
-      .then((data) => setCards(data));
-
-    fetch(nobles_url)
-      .then((res) => res.json())
-      .then((data) => setNobles(data));
-
     fetchInitialState();
   }, []);
+
+  useEffect(() => {
+    if (isFetching) {
+      nobleRefs.current.slice(1).forEach((el) => {
+        animateToUnderside(el!, nobleRefs.current[0]!);
+      });
+      cardRefs.current.forEach((row, i) => {
+        row.forEach((el) => animateToUnderside(el!, undersideRefs.current[i]!));
+      });
+    } else {
+      nobleRefs.current.forEach((el) => {
+        if (el) {
+          el.style.transform = "none";
+          el.style.filter = "none";
+        }
+      });
+      undersideRefs.current!.forEach((el) => {
+        if (el) {
+          el.style.transform = "none";
+          el.style.filter = "none";
+        }
+      });
+      cardRefs.current.forEach((row) => {
+        row.forEach((el) => {
+          if (el) {
+            el.style.transform = "none";
+          }
+        });
+      });
+    }
+  }, [isFetching]);
+
+  function animateToUnderside(cardEl: HTMLElement, undersideEl: HTMLElement) {
+    const cardRect = cardEl.getBoundingClientRect();
+    const underRect = undersideEl.getBoundingClientRect();
+
+    const dx = underRect.left - cardRect.left;
+    const dy = underRect.top - cardRect.top;
+
+    cardEl.style.transition = "transform 0.4s cubic-bezier(.2, .8, .2, 1)";
+    cardEl.style.transform = `translate(${dx}px, ${dy}px) scale(0.7)`;
+
+    undersideEl.style.transition = "transform 0.4s cubic-bezier(.2, .8, .2, 1)";
+    undersideEl.style.transform = "scale(1.05) rotate(5deg)";
+    undersideEl.style.filter = "saturate(.5)";
+  }
 
   return (
     <div className="flex h-screen bg-gray-100  flex-col justify-center items-center p-6">
@@ -62,7 +106,61 @@ export default function InitialGameStatePage() {
         Reshuffle Board
       </button>
 
-      {/* <NobleCard nobleInfo={gameState.} /> */}
+      {/* shop */}
+      <div className="w-4/5 aspect-[16/9] flex flex-row">
+        {/* cards & nobles */}
+        <div className="h-full flex flex-wrap bg-[#d1d5db] p-8 rounded-2xl">
+          <div className="h-full">
+            {/* TODO: convert this to a ternary if we have a loading state for cards & nobles */}
+            <div className="flex h-1/6 justify-center gap-4">
+              {gameState &&
+                gameState.nobles.map((noble, i) => (
+                  <div
+                    ref={(el) => {
+                      nobleRefs.current[i] = el;
+                    }}
+                    style={{ zIndex: `${4 - i}` }}
+                  >
+                    <NobleCard key={i} nobleInfo={noble} />
+                  </div>
+                ))}
+            </div>
+
+            <div className="flex flex-col h-full gap-y-2 mt-8">
+              {["t3", "t2", "t1"].map((tier, i) => (
+                <div className="flex h-1/4 justify-center gap-x-2">
+                  {gameState?.cards[tier as TierKey].revealed[0] && (
+                    <div
+                      ref={(el) => {
+                        undersideRefs.current[i] = el;
+                      }}
+                      style={{ zIndex: 999 }}
+                    >
+                      <EngineUnderside tier={2 - i} />
+                    </div>
+                  )}
+                  {gameState &&
+                    gameState.cards[tier as TierKey].revealed.map((card, j) => (
+                      <div
+                        ref={(el) => {
+                          if (!cardRefs.current[i]) cardRefs.current[i] = [];
+                          cardRefs.current[i][j] = el;
+                        }}
+                        className="relative transform-gpu"
+                        style={{ zIndex: `${4 - j}` }}
+                      >
+                        <EngineCard key={j} cardInfo={card} />
+                      </div>
+                    ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* bank */}
+        <div className="w-1/3 mx-auto flex flex-col bg-slate-800  rounded-2xl"></div>
+      </div>
     </div>
   );
 }
