79ee027453bede89a42462cee28cf6ee1024c713
knee-son <okaokaloid@gmail.com>
2025-11-17 06:54:53 +0800

    init GemCoin component

---
+++ frontend-ministry/src/components/GemCoin.tsx
@@ -0,0 +1,37 @@
+import type { CoinType } from "@/types/splendor";
+
+import diamondGem from "../assets/images/diamond_gem.png";
+import emeraldGem from "../assets/images/emerald_gem.png";
+import rubyGem from "../assets/images/ruby_gem.png";
+import sapphireGem from "../assets/images/sapphire_gem.png";
+import onyxGem from "../assets/images/onyx_gem.png";
+import goldLump from "../assets/images/gold_lump.png";
+
+interface CoinProps {
+  coinType: CoinType;
+}
+
+const coinImages: Record<CoinType, string> = {
+  diamond: diamondGem,
+  sapphire: sapphireGem,
+  emerald: emeraldGem,
+  ruby: rubyGem,
+  onyx: onyxGem,
+  gold: goldLump,
+};
+
+export default function NobleCard({ coinType }: CoinProps) {
+  const gemImage = coinImages[coinType];
+
+  return (
+    <div className="absolute bottom-2 left-2 text-lg rounded-md">
+      <div className="flex flex-col">
+        <img
+          src={gemImage}
+          alt={coinType}
+          className="w-1/3 h-1/3 object-contain"
+        />
+      </div>
+    </div>
+  );
+}
